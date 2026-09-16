2d69288ad14516bb07ee3bd902e0fc55cf50a0a3
Hà Mạnh Hùng <dtc245200936@ictu.edu.vn>
2026-09-16 10:11:24 +0700

    Thêm dữ liệu cho bảng QuanLySinhVien

--- quanlysinhvien.sql
+++ quanlysinhvien.sql
@@ -42,3 +42,29 @@ CREATE TABLE Mark (
     FOREIGN KEY (SubId) REFERENCES Subject (SubId),
     FOREIGN KEY (StudentId) REFERENCES Student (StudentId)
 );
+USE QuanLySinhVien;
+
+-- 1. Thêm dữ liệu vào bảng Class
+INSERT INTO Class (ClassID, ClassName, StartDate, Status)
+VALUES (1, 'A1', '2008-12-20', 1),
+       (2, 'A2', '2008-12-22', 1),
+       (3, 'B3', CURRENT_DATE, 0);
+
+-- 2. Thêm dữ liệu vào bảng Student
+INSERT INTO Student (StudentName, Address, Phone, Status, ClassId)
+VALUES ('Hung', 'Ha Noi', '0912113113', 1, 1),
+       ('Hoa', 'Hai phong', NULL, 1, 1),
+       ('Manh', 'HCM', '0123123123', 0, 2);
+
+-- 3. Thêm dữ liệu vào bảng Subject
+INSERT INTO Subject (SubID, SubName, Credit, Status)
+VALUES (1, 'CF', 5, 1),
+       (2, 'C', 6, 1),
+       (3, 'HDJ', 5, 1),
+       (4, 'RDBMS', 10, 1);
+
+-- 4. Thêm dữ liệu vào bảng Mark
+INSERT INTO Mark (SubId, StudentId, Mark, ExamTimes)
+VALUES (1, 1, 8, 1),
+       (1, 2, 10, 2),
+       (2, 1, 12, 1);
